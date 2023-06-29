9f9634a2c1853f5d090792378752d384a18d899e
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-06-29 10:07:08 +0530

    django admin list users

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -26,7 +26,7 @@ class AdminSiteTests(TestCase):
 
     def test_users_list(self):
         """Test that users are listed on page."""
-        url = reverse('admin:core_user_changeList')
+        url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
         self.assertContains(res, self.user.name)
