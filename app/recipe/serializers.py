3b266196369e034ede7ec0bb99b98896ace55a3f
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-07-06 13:36:53 +0530

    implemented image api

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -94,3 +94,13 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
+
+
+class RecipeImageSerialier(serializers.ModelSerializer):
+    """Serializer for uploading images to recipe."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'image']
+        read_only_fields = ['id']
+        extra_kwargs = {'image': {'required': 'True'}}
